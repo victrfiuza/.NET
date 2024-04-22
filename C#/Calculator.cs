a061dbf0800beb54af7ac71cbade4a89e15ebae6
victrfiuza <victorfiuza@icloud.comgit config --global user.name>
2024-04-22 17:39:59 -0300

    Adicionar arquivos de projeto.

---
+++ C#/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class Calculator
+    {
+        public int Calculate(string op, int value1, int value2)
+        {
+            if (op == "+")
+            {
+                return value1 + value2;
+            }
+
+            else if (op == "-")
+            {
+                return value1 - value2;
+            }
+
+            else if (op == "*") 
+            {
+                return value1 * value2;
+            }
+
+            else if(op == "/") 
+            {
+                return value1 / value2;
+            }
+            return 0;
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*  
+string myString = "Hello, World!";
+int myInt = 123;
+DateTime myDateTime = DateTime.Now;
+bool myBool = false;
+    
+Console.WriteLine(myBool);
+Console.ReadKey();
+
+------------------------------------------
+
+var myName = "Victor";
+var myAge = $"{myName}21";
+
+Console.WriteLine(myAge);
+
+------------------------------------------
+*/
+
+using C_;
+var age = int.Parse(Console.ReadLine());
+var operate = Console.ReadLine();
+var age2 = int.Parse(Console.ReadLine());
+
+Calculator calculator = new Calculator();
+Console.WriteLine(calculator.Calculate(operate, age, age2));
+Console.ReadLine();
